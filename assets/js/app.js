b14ed311b16a8649afc8a4240ad478527ec35b31
Jonathan Costa <jonathanduartecosta@gmail.com>
2024-04-16 21:53:46 -0300

    aplicando a responsividade

--- assets/js/app.js
+++ assets/js/app.js
@@ -521,7 +521,7 @@ cardapio.metodos = {
 // O QUE SERÁ ESCRITO NO HTML
 cardapio.templates = {
   item: `
-      <div class="col-3 mb-5 animated fadeInUp">
+      <div class="col-12 col-lg-3 col-md-3 col-sm-6 mb-5 animated fadeInUp">
         <div class="card card-item" id="\${id}">
           <div class="img-produto">
             <img src="\${img}" alt="">
@@ -551,7 +551,7 @@ cardapio.templates = {
           <span class="btn-menos" onClick="cardapio.metodos.diminuirQuantidadeCarrinho('\${id}')" ><i class="fas fa-minus"></i></span>
           <span class="add-numero-itens" id="qntd-carrinho-\${id}">\${qntd}</span>
           <span class="btn-mais" onClick="cardapio.metodos.aumentarQuantidadeCarrinho('\${id}')" ><i class="fas fa-plus"></i></span>
-          <span class="btn btn-remove" onClick="cardapio.metodos.removerItemCarrinho('\${id}')" ><i class="fa fa-times"></i></span>
+          <span class="btn btn-remove no-mobile" onClick="cardapio.metodos.removerItemCarrinho('\${id}')" ><i class="fa fa-times"></i></span>
         </div>
       </div>
   `,
